93f8e5427a3a77246df506eaefd766220f11d7e4
saadetsimsek <saadetsimsek13@gmail.com>
2024-10-06 20:14:10 +0300

    Initial Commit

---
+++ PhotoSearchTests/PhotoSearchTests.swift
@@ -0,0 +1,17 @@
+//
+//  PhotoSearchTests.swift
+//  PhotoSearchTests
+//
+//  Created by Saadet Şimşek on 06/10/2024.
+//
+
+import Testing
+@testable import PhotoSearch
+
+struct PhotoSearchTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
